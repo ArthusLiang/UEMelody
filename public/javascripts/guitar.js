416102e18613e2b98cf2ba9cb4d6f3ade741c71b
Arthus <yulhuang@cisco.com>
2014-12-12 14:46:44 +0800

    add some thing
    aaaaaa

--- public/javascripts/guitar.js
+++ public/javascripts/guitar.js
@@ -1,35 +1,173 @@
 (function(){
 
-    var guitar = {};
+    var Guitar ={},
+        Core = new function(){
+            this.extend=function(target,source){
+                var args=[].slice.call(arguments),
+                    i=1,
+                    key,
+                    ride= typeof args[args.length-1] == 'boolean' ? args.pop() : true;
+                if(args.length==1){
+                    target = !this.window ? this: {};
+                    i=0;
+                }
+                while((source=args[i++])){
+                    for(key in source){
+                        if(ride || !(key in target)){
+                            target[key] =source[key];
+                        }
+                    }
+                }
+                return target;
+            };
+            this.isArray=function (obj) {
+                return Object.prototype.toString.call(obj) === '[object Array]';
+            };
+        };
 
-    //Major minor
-
+    /*
+    * @namespace   MusicScore
+    * @Author:     yulianghuang
+    * @CreateDate  2014/12/12
+    * @Desciption  Record Music Score
+    */
     (function(){
-        var A1=440,
-            MusicalAlphabet = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B'],
+        var MusicalAlphabet = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B'],
             RollCallName=['do','re','mi','fa','sol','la','si'],
-            RollCall= [1,2,3,4,5,6,7],
+            TuneInterval={
+                major:[2,2,1,2,2,2,1],
+                minor:[2,1,2,2,1,2,2],
+                major5:[2,2,3,2,3],
+                minor5:[2,3,2,3,2]
+            },
+            RollCall= [1,2,3,4,5,6,7],//0 is special standard for no sound
             freqChat={},
-            freqRange=7;
+            freqRange=7,//C1-C7
+            regBeat=/^\d\/\d/,
+            _oneBeat=24,// the length of one beat
+            i,j,base;
 
-        var Tune = function(){
+        //init freq chat
+        for(i=1;i<freqRange;i++){
+            freqChat[i]={};
+            base = (i-1)*12;
+            for(j=0;j<12;j++){
+                //A1=440
+                freqChat[i][MusicalAlphabet[j]]=440*Math.pow(2,(base+j-9)/12);
+            }
+        }
 
+        /*
+        * @param rollcall number in [1,2,3,4,5,6,7]
+        * @param duration number 1/4,1/16,1/8,1/12,1/2,1
+        * @param hasDot boolen
+        */
+        var Note =function(rollcall,duration,hasDot){
+            this.Rollcall = rollcall;
+            this.Duration = duration;
+            this.HasDot = hasDot;
+            this._len = duration * 96 * (hasDot ? 1.5 : 1);//duratuib * 4 * 24
+            this.IsPart = false;
         };
-        Tune.prototype={
-            FreqChat:freqChat
+
+        /*
+        # @param 3/4 4/4 6/8
+        * @param numerator number
+        * @param denominator number
+        */
+        var Section=function(numerator,denominator){
+            this.Numerator = numerator;
+            this.Denominator = denominator;
+            this._len = numerator/denominator*96;
+            this.Data=[];
+        };
+        Section.prototype={
+            add:function(note){
+                if(note instanceof of Note){
+
+                }
+            },
+            remove:function(pIndex){
+
+            }
         };
 
-        guitar.Tune = Tune;
+
+
+        //var tune = new MusicScore('C','major','4/4');
+        var MusicScore = function(alphabet,intervalName,beat){
+            var me =this,
+                _intervalName = Core.isArray(TuneInterval[intervalName]) ? intervalName : 'major',
+                _alphabet = MusicalAlphabet[alphabet]!=undefined ? alphabet : 'C';
+                _beat = regBeat.test(beat) || '4/4',
+                _beatArr = _beat.split('/');
+
+            me.Mode={
+                Alphabet:_alphabet,
+                Interval:_intervalName,
+                Name:_alphabet+_intervalName,
+                Beat:_beat,
+                Numerator:_beatArr[0],
+                Denominator:_beatArr[1]
+            };
+            me.Data=[];
+        };
+        MusicScore.prototype={
+            FreqChat:freqChat,
+            w:function(note){
+                if(note instanceof of Note){
+                    this.Data.push(note);
+                }
+            },
+            wrtieSection:function(numerator,denominator){
+
+            },
+            copy:function(){
+
+            },
+            print:function(){
+
+            },
+            compile:function(){
+
+            }
+        };
+
+        Guitar.MusicScore = MusicScore;
 
     })();
 
 
+    (function(){
 
-    guitar.Board = function(){
+        var Player = function(){
 
-    };
-    guitar.Board.prototype=function(){
+        };
+        Player.prototype=function(){
 
-    };
+        };
+
+    })();
+
+    (function(){
+        var Board = function(canvas){
+            this.canvas;
+        };
+        Board.prototype=function(){
+            draw:function(){
+
+            },
+            background:function(){
+
+            }
+        };
+
+        Guitar.Board = Board;
+    })();
+
+
+
+
+    window.Guitar =Guitar;
 
 })();
